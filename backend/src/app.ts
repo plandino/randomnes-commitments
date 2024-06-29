b8ecdfcecd72e6b1fe45dee2c8397c7a4f691b80
-h <plandino@hotmail.com>
2024-06-29 14:21:26 -0300

    adds drand library

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,15 @@
+import { fetchBeacon, HttpCachingChain, HttpChainClient } from 'drand-client';
 import express from 'express';
+
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/', async (req, res) => {
+  const chain = new HttpCachingChain('https://api.drand.sh/52db9ba70e0cc0f6eaf7803dd07447a1f5477735fd3f661792ba94600c84e971');
+  const client = new HttpChainClient(chain);
+  const latestBeacon = await fetchBeacon(client);
+  res.send(`The latest beacon is ${latestBeacon.randomness}`);
 });
 
 app.listen(port, () => {
